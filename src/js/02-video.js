96d8a7b1c8abe9c040c0d6e9c5d01288b87e231e
Beniamin Brzeziński <benek9311@wp.pl>
2023-02-23 18:18:43 +0100

    Did a few things in the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+const savePlayTime = e => {
+  try {
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(e));
+  } catch (error) {
+    console.error('Set state error:', error.stack);
+  }
+};
+
+player.on('timeupdate', savePlayTime);
+
+try {
+  const currentTimeInSeconds = JSON.parse(
+    localStorage.getItem(LOCALSTORAGE_KEY)
+  ).seconds;
+  player.setCurrentTime(currentTimeInSeconds);
+} catch (error) {
+  console.error('Get state error:', error.stack);
+}
