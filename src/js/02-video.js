0d6d74c416586b60b2aa17e43fc7ead3e900c828
Beniamin Brzeziński <benek9311@wp.pl>
2023-02-24 13:58:22 +0100

    Completion of the second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
-const savePlayTime = e => {
+const savePlayTime = throttle(e => {
   try {
     localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(e));
   } catch (error) {
     console.error('Set state error:', error.stack);
   }
-};
+}, 1000);
 
 player.on('timeupdate', savePlayTime);
 
